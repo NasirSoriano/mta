b91eb39f41ee2e9b5978192bb7e49cb35b2b92bf
NasirSoriano <152217973+NasirSoriano@users.noreply.github.com>
2023-12-04 23:49:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script para la interactividad
+
+document.getElementById('gtaCard').addEventListener('click', function() {
+    alert('Haz clic en la tarjeta de GTA SA');
+});
+
+document.getElementById('mtaCard').addEventListener('click', function() {
+    alert('Haz clic en la tarjeta de MTA y SAMP');
+});
